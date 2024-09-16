59542f121d407a4cf8f8346997e9d4f8102f8030
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-09-15 21:35:39 -0300

    refactor run parameters into own files, and object model and caption models into a class file

---
+++ models.py
@@ -0,0 +1,61 @@
+import torch
+
+from transformers import Owlv2Processor, Owlv2ForObjectDetection
+
+class Owl2:
+  OBJ_TARGET_SIZE = torch.Tensor([500, 500])
+  OBJ_MODEL = "google/owlv2-base-patch16-ensemble"
+
+  obj_model = Owlv2ForObjectDetection.from_pretrained(OBJ_MODEL).to("cuda")
+  obj_processor = Owlv2Processor.from_pretrained(OBJ_MODEL)
+
+  @staticmethod
+  def px_to_pct(box, img_w, img_h):
+    model_dims = Owl2.OBJ_TARGET_SIZE
+    scale_factor = torch.tensor([max(img_w, img_h) / img_w , max(img_w, img_h) / img_h])
+    return [round(x, 4) for x in (box.cpu().reshape(2, -1) / model_dims * scale_factor).reshape(-1).tolist()]
+
+  @staticmethod
+  # filter if box "too large" or "too small"
+  def threshold(score, label, box, tholds, img_w, img_h):
+    box_pct = Owl2.px_to_pct(box, img_w, img_h)
+    box_width = box_pct[2] - box_pct[0]
+    box_height = box_pct[3] - box_pct[1]
+    good_min = box_width > 0.05 and box_height > 0.05
+    good_max = box_width < 0.8 or box_height < 0.8
+    return good_min and good_max and score > tholds[label.item()]
+
+  @staticmethod
+  def run_object_detection(img, labels_in, labels_out, tholds):
+    input = Owl2.obj_processor(text=labels_in, images=img, return_tensors="pt").to("cuda")
+    with torch.no_grad():
+      obj_out = Owl2.obj_model(**input)
+
+    res = Owl2.obj_processor.post_process_object_detection(outputs=obj_out, target_sizes=[Owl2.OBJ_TARGET_SIZE])
+    slbs = zip(res[0]["scores"], res[0]["labels"], res[0]["boxes"])
+    iw, ih = img.size
+
+    detected_objs = [{"score": s, "label": labels_out[l.item()], "box": Owl2.px_to_pct(b, iw, ih)}
+                     for s,l,b in slbs if Owl2.threshold(s, l, b, tholds, iw, ih)]
+    return detected_objs
+
+  @staticmethod
+  def top_objects(img, labels_in, labels_out, tholds):
+    detected_objs = Owl2.run_object_detection(img, labels_in, labels_out, tholds)
+
+    # only keep the box with highest score per object
+    detected_objs_boxes = {}
+    high_score = {}
+
+    for o in detected_objs:
+      ol = o["label"]
+      if (ol not in detected_objs_boxes) or (o["score"] > high_score[ol]):
+        detected_objs_boxes[ol] = o["box"]
+        high_score[ol] = o["score"]
+  
+    return detected_objs_boxes
+
+  @staticmethod
+  def all_objects(img, labels_in, labels_out, tholds):
+    detected_objs = Owl2.run_object_detection(img, labels_in, labels_out, tholds)
+    return detected_objs
---
+++ parameters/finetune_0915.py
@@ -0,0 +1,37 @@
+IMAGES_PATH = "../../imgs/arquigrafia"
+OBJECTS_PATH = "./metadata/json/objects-ft-0915"
+CAPTIONS_PATH = "./metadata/json/captions-ft-0915"
+
+DB_FILE_PATH = "./metadata/json/objects-ft-0915.json"
+
+OBJECTS = [
+  {
+    "inclined walkway": 0.32,
+    "pedestrian ramp": 0.25,
+    "ramp": 0.8,
+    "door overhang": 0.25,
+    "awning": 0.25,
+    "balcony": 0.25,
+    "window deck": 0.25,
+    "street sign": 0.25,
+    "placard": 0.25,
+    "signboard": 0.25,
+    "billboard": 0.25,
+  },
+]
+
+OBJECT2LABEL = {}
+
+LABEL2DISPLAY = {
+  "inclined walkway": "inclined walkway",
+  "pedestrian ramp": "pedestrian ramp",
+  "ramp": "ramp",
+  "door overhang": "door overhang",
+  "awning": "awning",
+  "balcony": "balcony",
+  "window deck": "window deck",
+  "street sign": "street sign",
+  "placard": "placard",
+  "signboard": "signboard",
+  "billboard": "billboard",
+}
